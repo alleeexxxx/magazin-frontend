af96da413734cba2521bfe46eb543b0dc966ef9f
alleeexxxx <9782452@gmail.com>
2025-03-20 14:31:31 +0300

    Add Fire component, Header and routing

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import Wave from './components/Wave';
+import Fire from './components/Fire';
+import Header from './components/Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} />
-      <OrbitControls />
-      <Suspense fallback={null}>
-        <Wave count={100} position={[0, 0, 0]} />
-      </Suspense>
-    </Canvas>
+    <Router>
+      <Header />
+      <Canvas style={{ width: '100vw', height: '100vh', margin: 0, overflow: 'hidden' }}>
+        <ambientLight intensity={0.5} />
+        <pointLight position={[10, 10, 10]} />
+        <OrbitControls />
+        <Suspense fallback={null}>
+          <Routes>
+            <Route path="/" element={<Wave count={100} position={[0, 0, 0]} />} />
+            <Route path="/fire" element={<Fire count={100} />} />
+          </Routes>
+        </Suspense>
+      </Canvas>
+    </Router>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Waves</Link>
+        </li>
+        <li>
+          <Link to="/fire">Fire</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Header;
